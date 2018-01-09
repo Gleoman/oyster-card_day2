1a917a0547b782524be35c2b714bead938c01890
Ellie <elliewem@gmail.com>
2018-01-09 16:08:59 +0000

    Adds station.rb and station_spec.rb

---
+++ spec/station_spec.rb
@@ -0,0 +1,16 @@
+require 'station'
+
+RSpec.describe Station do
+
+    it 'initializes station with zone 1' do
+      station = Station.new("Aldgate", 1)
+      expect(station.zone).to eq 1
+    end
+
+    it 'initializes station with a name' do
+      station = Station.new("Aldgate", 1)
+      expect(station.name).to eq "Aldgate"
+    end
+
+
+end
